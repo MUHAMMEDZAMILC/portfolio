787c491ab46d10a36323a6e739b6111a46ff155c
MUHAMMEDZAMILC <mzofficial049@gmail.com>
2023-10-14 08:23:57 +0530

    update 5

--- js/mail.js
+++ js/mail.js
@@ -1,41 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC85FsUOVKM-n3kuP5YUbw0cyKrnqUJ-QI",
-    authDomain: "portfolio-dcc50.firebaseapp.com",
-    projectId: "portfolio-dcc50",
-    storageBucket: "portfolio-dcc50.appspot.com",
-    messagingSenderId: "409175514758",
-    appId: "1:409175514758:web:008c61374d594477023b07",
-    measurementId: "G-E5XDC2MMTN"
-  };
-// initilize firebase
-firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyCVQEa3af7N1F54NaWOQ2rp6zoaZZDT_KU",
+    authDomain: "portfolio-b96b4.firebaseapp.com",
+    projectId: "portfolio-b96b4",
+    storageBucket: "portfolio-b96b4.appspot.com",
+    messagingSenderId: "103584299208",
+    appId: "1:103584299208:web:5fb811bed265b767a60dfd",
+    measurementId: "G-Y9DPJEP6QG"
+};
 
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // reference your database
 const portfolio = firebase.firestore().collection("collectionss");
-document.getElementById("send").addEventListener("submit",subitForm);
+document.getElementById("send").addEventListener("submit", subitForm);
 
-function subitForm(e){
+function subitForm(e) {
     e.preventDefault();
     var fname = getElementVal('fname');
     var email = getElementVal('email');
     var subject = getElementVal('subject');
     var message = getElementVal('message');
-    // console.log(fname,email,subject,message);
-    saveMessage(fname,email,subject,message);
+    console.log(fname, email, subject, message);
+    saveMessage(fname, email, subject, message);
 }
 
-const saveMessage = (fname,email,message,subject)=>{
+const saveMessage = (fname, email, message, subject) => {
     // var newContactForm = portfolio.push();
     portfolio.set({
-        'fname' : fname,
-        'email' :email,
-        'subject' : subject,
-        'message' : message,
-        'status' : 1,
+        'fname': fname,
+        'email': email,
+        'subject': subject,
+        'message': message,
+        'status': 1,
     });
 };
 
-const getElementVal = (id)=>{
+const getElementVal = (id) => {
     return document.getElementById(id).value;
 }
